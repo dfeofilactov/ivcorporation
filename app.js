86a86c40ed1255234c18cdc9c18d8e6b82523290
Denys Feofilaktov <denis.feofilaktov@outlook.com>
2019-07-30 12:09:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 
+const PORT = process.env.PORT || 8080;
 const app = express();
-
 const router = express.Router();
+
 router.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirname, './docs') }));
 
 app.use(logger('dev'));
 app.use('/', router);
 app.use(express.static(path.join(__dirname, './docs', '/')));
 
-app.listen(3000, () => {
-    console.log('IV Corp listening on port 3000!');
+app.listen(PORT, () => {
+    console.log(`IV Corp listening on port: ${ PORT }!`);
 });
