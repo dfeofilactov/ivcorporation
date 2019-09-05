a446b770294a336ffba1dbee62670234cc3bdbef
AnthonyRokochy <lekennedyon@gmail.com>
2019-09-05 17:09:52 +0300

    FirstTry

---
+++ front/components/MainPage/Menu/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import classnames from 'classnames';
+class Menu extends Component {
+    render() {
+        return (
+            <div className='Menu'>
+                <img src='ivcorp.png' alt='' />
+                asdasdasd
+                asdasdasdasdas
+            </div>
+        );
+    }
+}
+
+//SomeComponent.propTypes = {
+//
+//};
+
+//SomeComponent.defaultProps = {
+//
+//};
+
+// function select(/* store */) {
+//     return { };
+// }
+
+export default Menu;
