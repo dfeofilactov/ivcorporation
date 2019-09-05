f8a4542d65fc75dfeccceba4f3862fc255eb10f7
Denys Feofilaktov <denis.feofilaktov@outlook.com>
2019-09-05 19:22:15 +0300

    mouse events

--- front/components/App.js
+++ front/components/App.js
@@ -1,12 +1,25 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import classnames from 'classnames';
 import { withRouter } from 'react-router-dom';
+import Scroller from 'react-custom-scrollbars';
+// import Scroller from 'react-custom-scroll';
+// import Scroller from 'react-perfect-scrollbar';
 
 import { OPEN } from '../redux/actions/actions';
 import Header from './MainPage/Header';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            thumbGrab: false,
+            //
+        };
+        this.onThumbMouseUp = :: this.onThumbMouseUp;
+        this.onThumbMouseDown = :: this.onThumbMouseDown;
+    }
     componentDidMount() {
         const isMobile = window.screen.width < 600;
         if (isMobile) console.log('[VERSION] MOBILE');
@@ -17,20 +30,42 @@ class App extends Component {
             }, 1000);
         };
     }
+    onThumbMouseUp() {
+        console.log('mouse up');
+        this.setState({ thumbGrab: false });
+    }
+    onThumbMouseDown() {
+        console.log('mouse up');
+        this.setState({ thumbGrab: true });
+    }
     render() {
-        const { dict } = this.props;
+        // const { dict } = this.props;
+        const { thumbGrab } = this.state;
         return (
-            <>
-                <div className='AppContainer'>
+            <div className='AppContainer'>
+                <Scroller
+                    className='Scroller'
+                    renderThumbVertical={ props => (
+                        <div
+                            role='presentation'
+                            className={ classnames('thumb', { 'grab': thumbGrab }) }
+                            onMouseDown={ this.onThumbMouseDown }
+                            onMouseUp={ this.onThumbMouseUp }
+                            { ...props }
+                        />
+                    ) }
+                    renderTrackVertical={ props => <div { ...props } className='track' /> }
+                    /* heightRelativeToParent='100%' */
+                >
                     <Header />
-                </div>
-            </>
+                </Scroller>
+            </div>
         );
     }
 }
 App.propTypes = {
     dispatch: PropTypes.func.isRequired,
-    dict: PropTypes.object.isRequired,
+    // dict: PropTypes.object.isRequired,
 };
 
 function select(store) {
