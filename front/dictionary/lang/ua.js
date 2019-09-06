d0679d67dee0b4accb2115ecb40115d3cccebd3c
AnthonyRokochy <lekennedyon@gmail.com>
2019-09-06 15:47:58 +0300

    Add ua.js

---
+++ front/dictionary/lang/ua.js
@@ -0,0 +1,14 @@
+export default {
+    'modules': {
+        'main': 'Головна',
+        'legal_services': 'Послуги для юр.осіб',
+        'individual_services': 'Послуги для фіз.осіб',
+        'addition_services': 'Додаткові послуги',
+        'contacts': 'Контакти',
+        'about': 'Про мене',
+    },
+    'test': {
+        'greeting': 'ЦЕ МАТЬ ЙОГО СПАРТА!!!',
+        // empty line
+    },
+};
