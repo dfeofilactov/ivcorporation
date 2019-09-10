38f4ca59513c31252653c0c011baab8f7d78ecde
Denis <denis.feofilaktov@outlook.com>
2019-09-11 00:28:28 +0300

    fix

--- front/consts/generalConsts.js
+++ front/consts/generalConsts.js
@@ -12,5 +12,5 @@ export const ADDITION_SERVICES = 'addition_services';
 export const CONTACTS = 'contacts';
 export const MODULE_TYPE_DARK = 'dark_module';
 
-export const IMG_GREETINGS_URL = 'https://1fz4hg.db.files.1drv.com/y4myQAM-8UPwfYoCi0aAI_T_ym5WGp4EvpWwvbyBE212TsjwI87WO_ePH91FwO6PIuEl3PoWcRMMjQgg7ujMzGx_34F78xI81qy8hBb8qRHqqpWi0uah1izSlVDlqUA69U35GjqhU3eQXUoqo72pvi8ZGl7BTn6PiVUEKhgBJzAUZfrItCRRm8yOyJoax8Tx0oFG03qCkU5ATFFJo-BRV2Fiw?width=3449&height=2421&cropmode=none';
+export const IMG_GREETINGS_URL = 'https://0fz4hg.db.files.1drv.com/y4mIB6uA54y-Q7EIyV39VWmiPwnTWYCUvuKc-es84OPYzsD3dD_673JHdT_1Pyo2DoHNaa2oltEocHcHM3vtFVto8wzDvRvFa30LYr5O6nmsrAdRZAw2YCthtCij-tfjpE-XqBqMIZnopZPj1xOSi5O3X5858pETyYu-9gcEP-aNgTrW_4W_JXB2zhtgu3_Z2z0SV3WZsUmDxmpDTXDLLPizw?width=4896&height=3264&cropmode=none';
 export const IMG_IMPORTANT_BGR_URL = 'https://i.imgur.com/P294MGn.jpg';
