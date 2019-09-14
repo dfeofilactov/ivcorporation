e8848740bab73b2a74356f158fa68e55b2e53cbb
anton <lekennedyon@gmail.com>
2019-09-15 00:11:49 +0300

    Add Contacts Bottom Block

--- front/components/Contacts/index.js
+++ front/components/Contacts/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ContactsBlock from './ContactsBlock';
+import ContactsLink from './ContactsLink';
 // import classnames from 'classnames';
 class Contacts extends Component {
     render() {
@@ -9,6 +10,15 @@ class Contacts extends Component {
             <div className='Contacts'> 
                 <div className='ContactsContainer'>
                    <ContactsBlock />
+                   <ContactsLink />
+                   <ContactsLink />
+                   <ContactsLink />
+                </div>
+                <div className='ContactsCredits'>
+                        <div className='ContactsRightsName'>Інна Василівська.&nbsp;</div>
+                        <div className='ContactsRightsExplain'>Адвокат. Всі права захищено. Україна, Київ, 2019.</div>
+                        <div className='Design'>Designed by &nbsp;</div>
+                        <div className='Creator'>Denis Feofilaktov</div>
                 </div>
             </div>
         );
