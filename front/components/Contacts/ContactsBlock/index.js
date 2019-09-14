31c8f94dc95b465effb02de7f40a21d96e3396e5
anton <lekennedyon@gmail.com>
2019-09-14 23:26:52 +0300

    Made LogoType Component

--- front/components/Contacts/ContactsBlock/index.js
+++ front/components/Contacts/ContactsBlock/index.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import LogoType from '../../LogoType';
 // import classnames from 'classnames';
 class ContactsBLock extends Component {
     render() {
         return (
             <div className='ContactsBlock'>
-                <img src='https://live.staticflickr.com/65535/48701108292_210a00bb9d_o.png' alt='' />
-                <div>Україна, м.Київ</div>
+                <LogoType />
+                <div className='Adress'>Україна, м.Київ</div>
                 <div className='DarkContactsText'>vasylivska.inna@gmail.com</div>
-                <div className='DarkContactsText'>+38 (095) 322-23-23</div>
-                <div className='DarkContactsText'>+38 (095) 322-23-23</div>
-                <img src='http://pngimg.com/uploads/telegram/telegram_PNG22.png' alt='' />
-                <img src='http://pngimg.com/uploads/twitter/twitter_PNG9.png' alt='' />
-                <img src='https://i.pinimg.com/originals/41/28/2b/41282b58cf85ddaf5d28df96ed91de98.png' alt='' />
+                <div className='DarkContactsText'>
+                    <div>+38 (095) 322-23-23</div>
+                    <div>+38 (095) 322-23-23</div>
+                </div>
+                <div className='SocialNetworksIMG'>
+                    <img src='http://pngimg.com/uploads/telegram/telegram_PNG22.png' alt='' />
+                    <img src='http://pngimg.com/uploads/twitter/twitter_PNG9.png' alt='' />
+                    <img src='http://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19756.png' alt='' /> 
+                </div>
+                
             </div>
         );
     }
