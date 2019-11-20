25e0ef97544296849da4b11828e1c8ead51b5dfb
dfeofilaktov <denis.feofilaktov@outlook.com>
2019-11-20 18:34:24 +0200

    colors fix

--- front/components/LogoType/index.js
+++ front/components/LogoType/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 import { IMG_LOGO } from '../../consts/generalConsts';
-// import classnames from 'classnames';
+
 class LogoType extends Component {
     render() {
+        const { isMenuDark } = this.props;
         return (
-            <div className='LogoContain'>
+            <div className={ classnames('LogoType', { 'dark': isMenuDark }) }>
                 <div className='LogoIMG'>
                     <img src={ IMG_LOGO } alt='logo' />
                 </div>
@@ -19,16 +21,20 @@ class LogoType extends Component {
     }
 }
 
-//LogoType.propTypes = {
-//
-//};
+LogoType.propTypes = {
+    isMenuDark: PropTypes.bool.isRequired,
+    //
+};
 
 //LogoType.defaultProps = {
 //
 //};
 
-function select(/* store */) {
-    return { };
+function select(store) {
+    return {
+        isMenuDark: store.viewReducer.isMenuDark,
+        //
+    };
 }
 
 export default connect(select)(LogoType);
