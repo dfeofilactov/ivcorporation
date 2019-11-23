b5869af142e93640626e34632b5e08e99637a23c
Denis <denis.feofilaktov@outlook.com>
2019-11-23 11:15:10 +0200

    styles

--- front/consts/generalConsts.js
+++ front/consts/generalConsts.js
@@ -30,7 +30,7 @@ export const SERVICE_TYPE_ADDITIONAL = 'service_type_additional';
 export const LEGAL_SERVICES_TEXT = 'legal_services_text';
 
 // Images urls
-export const IMG_GREETINGS_URL = 'https://firebasestorage.googleapis.com/v0/b/ivcorporation-5c7f2.appspot.com/o/images%2Fmain5.jpg?alt=media&token=64baa778-30e0-48cb-84a0-f130b3867405';
+export const IMG_GREETINGS_URL = 'https://firebasestorage.googleapis.com/v0/b/ivcorporation-5c7f2.appspot.com/o/images%2Fphoto-1414322058660-a4c56ab6c1e2.jpeg?alt=media&token=aae89611-9999-4fd4-b908-001096ee2a14';
 export const IMG_IMPORTANT_BGR_URL = 'https://i.imgur.com/P294MGn.jpg';
 export const IMG_LOGO = 'https://i.imgur.com/OrbCM0u.png';
 export const IMG_ADDITIONAL_BGR_URL = 'https://3qz4hg.db.files.1drv.com/y4mb7vYaZGazk8RUhZUDr8kFdBCUdSnz2vcf7GZ33B-sUokaM44344uA1-DYgeHu0AOL3AiJjUNX3QMTePGf2Bax9OXGMFb8lhTD74Mk9kQouBjSfUEs0SNSRyl4J7ul-MSZ-aWMGXXuXLn-xJG4YFjcT86wvhm1C672kkk01xfG61uOMKHbA0Y41JFXv_l7LyAGUP7X2Pwe85PKupDComkJQ?width=6000&height=4000&cropmode=none';
